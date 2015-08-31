abfe40b3062527d3fbd120bbd90722f90a2b9ad9
donnaaboise <donna.calhoun@gmail.com>
2015-08-31 12:43:54 -0600

    Add flag2refine.f90 (mq = 1 only); add print options for schlieren plots

--- examples/euler_2d_shockbubble/afterframe.m
+++ examples/euler_2d_shockbubble/afterframe.m
@@ -1,4 +1,6 @@
-colormap(jet)
+if (PlotType ~= 3)
+    colormap(jet)
+end
 
 axis([0 2.0 0 0.5])
 axis equal; 
@@ -7,24 +9,72 @@ axis tight
 fprintf('%10s : %12.4f\n','qmin',qmin);
 fprintf('%10s : %12.4f\n','qmax',qmax);
 
-caxis([0.1 2.81]);
+if (PlotType == 3)
+    caxis([0 200]);
+else
+    caxis([0.1 2.81]);
+end
 
 showpatchborders
 setpatchborderprops('linewidth',1);
 hidegridlines(1:3);
-delete(get(gca,'title'));
-delete(get(gca,'xlabel'));
-delete(get(gca,'ylabel'));
+
 tstr = sprintf('Clawpack 5.0 : t = %8.4f\n',t);
 title(tstr,'fontsize',16);
+set(gca,'fontsize',16);
 
-prt = false;
+
+prt = true;
 NoQuery = 0;
 if (prt)
-    MaxFrames = 11;
-    fname = sprintf('sbamr_nomesh20_%2.2d.png',Frame);
-    disp(fname);
-    print('-dpng',fname);
+    MaxFrames = 31;
+    axis off;
+
+    id = input('Input id to use : ');
+    if (~isempty(id))
+        axis off;
+        delete(get(gca,'title'));
+        
+        set(gcf,'papersize',[4,1]);
+        set(gca,'position',[0 0 1 1]);
+        set(gcf,'paperposition',[0 0 4 1]);
+    
+        % With mesh
+        setpatchborderprops('linewidth',1);
+        hidegridlines;
+        if (PlotType == 3)
+            fname = sprintf('results_%03d/amr_sb_schlrn_%02d.png',id,Frame);            
+        else
+            fname = sprintf('results_%03d/amr_sb_%02d.png',id,Frame);
+        end
+        yn = 'y';
+        if (exist(fname))
+            str = sprintf('Are you sure you want to overwrite %s (y/[n]) ? ',...
+                fname);            
+            yn = input(str,'s');               
+            if isempty(yn)
+                yn = 'n';
+            end
+        end      
+        if (strcmp(lower(yn),'y') == 1)
+            fprintf('Printing %s\n',fname);
+            print('-r512','-dpng',fname);
+        end
+        
+        % no mesh
+        hidegridlines;
+        hidepatchborders;
+        if (PlotType == 3)
+            fname = sprintf('results_%03d/amr_sb_schlrn_nomesh_%02d.png',id,Frame);            
+        else
+            fname = sprintf('results_%03d/amr_sb_nomesh_%02d.png',id,Frame);
+        end
+        if (strcmp(lower(yn),'y') == 1)
+            fprintf('Printing %s\n',fname);
+            print('-r512','-dpng',fname);
+        end
+    end
+    
 end
 
 clear afterframe
